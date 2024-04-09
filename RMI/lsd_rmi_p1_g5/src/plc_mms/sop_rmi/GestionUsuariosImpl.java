cfab086f90f724b9255f84550c4252ff030c5d1a
Oscar Fernando Hoyos Carvajal <57969765+Viperexz@users.noreply.github.com>
2024-04-09 16:30:59 -0500

    Actualizacion impl

--- RMI/lsd_rmi_p1_g5/src/plc_mms/sop_rmi/GestionUsuariosImpl.java
+++ RMI/lsd_rmi_p1_g5/src/plc_mms/sop_rmi/GestionUsuariosImpl.java
@@ -2,9 +2,30 @@ package plc_mms.sop_rmi;
 
 import plc_mms.dto.Usuario_DTO;
 
+import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class GestionUsuariosImpl extends UnicastRemoteObject implements GestionUsuariosInt {
     private Usuario_DTO usuario;
-    private int 
+    private int sesionOPER;
+
+    public GestionUsuariosImpl() throws RemoteException
+    {
+
+    }
+
+    @Override
+    public int abrirSesion(Usuario_DTO objUsuario) throws RemoteException {
+        return 0;
+    }
+
+    @Override
+    public Usuario_DTO consultarUsuario(int identificacion) throws RemoteException {
+        return null;
+    }
+
+    int buscarAdmin(Usuario_DTO obj)
+    {
+        return 0;
+    }
 }
