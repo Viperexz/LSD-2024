edf36a50f2fe0dcfe6f8756c42adb6eaea568b0f
Oscar Fernando Hoyos Carvajal <57969765+Viperexz@users.noreply.github.com>
2024-04-18 16:17:29 -0500

    Preparacion entrega final

---
+++ RMI/lsd_rmi_archivos_fuente_g5/src/plc_tu/Login.java
@@ -0,0 +1,13 @@
+package plc_tu;
+
+import javax.swing.*;
+
+public class Login {
+    private JTextField txtUsuario;
+    private JButton btnLogin;
+    private JTextField txtClave;
+    private JTextField txtID;
+
+
+
+}
