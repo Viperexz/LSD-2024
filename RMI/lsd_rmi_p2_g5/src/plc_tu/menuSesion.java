88bb8104b4c4aa22a3e78ac02657272bd0f295c1
andresr02799 <anjaramirez@unicauca.edu.co>
2024-04-19 00:16:35 -0500

    Creacion de interfaces graficas de menus y registrar dispositivo

---
+++ RMI/lsd_rmi_p2_g5/src/plc_tu/menuSesion.java
@@ -0,0 +1,9 @@
+package plc_tu;
+
+import javax.swing.*;
+
+public class menuSesion {
+    private JButton a3SalirButton;
+    private JButton a1AbrirSesionButton;
+    private JButton a2MenuUsuarioButton;
+}
---
+++ RMI/lsd_rmi_p2_g5/src/plc_tu/menuUsuario.java
@@ -0,0 +1,8 @@
+package plc_tu;
+
+import javax.swing.*;
+
+public class menuUsuario {
+    private JButton a1ConsultarConsumoButton;
+    private JButton a2SalirButton;
+}
