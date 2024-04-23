60d8e0eb6680827ea49488200bae42caf8c2d9bf
andresr02799 <anjaramirez@unicauca.edu.co>
2024-04-23 16:32:00 -0500

    creacion de archivos callback cliente

---
+++ RMI/lsd_rmi_archivos_fuente_g5/src/plc_tu/sop_rmi/UsuarioCllbckInt.java
@@ -0,0 +1,11 @@
+package plc_tu.sop_rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface UsuarioCllbckInt extends Remote
+{
+    public void notificar(String mensaje, int cantidadUsuarios) throws RemoteException;
+
+}
+
